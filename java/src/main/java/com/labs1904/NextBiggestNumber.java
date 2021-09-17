3a71e1e79677c519e1ac93270339bd98e3cd31b5
Cynthia Holman <cynthiaholman@outlook.com>
2021-09-16 19:50:27 -0500

    adding java solution

--- java/src/main/java/com/labs1904/NextBiggestNumber.java
+++ java/src/main/java/com/labs1904/NextBiggestNumber.java
@@ -1,5 +1,8 @@
 package com.labs1904;
 
+
+import java.util.Arrays;
+
 public class NextBiggestNumber {
 
     public static void main(String[] args) {
@@ -10,8 +13,33 @@ public class NextBiggestNumber {
     }
 
     public static int getNextBiggestNumber(Integer i) {
-        //TODO: IMPLEMENT ME!
-        //test
-        return 0;
+
+        char[] number = (i + "").toCharArray();
+
+        int n, j;
+
+        for (n = number.length-1; n > 0; n--)
+            if (number[n-1] < number[n])
+                break;
+
+        if (n == 0)
+            return -1;
+
+
+        int x = number[n-1], smallest = n;
+        for (j = n+1; j < number.length; j++)
+            if (number[j] > x && number[j] <= number[smallest])
+                smallest = j;
+        r
+        char temp = number[n-1];
+        number[n-1] = number[smallest];
+        number[smallest] = temp;
+
+        Arrays.sort(number, n, number.length);
+
+        long val = Long.parseLong(new String(number));
+        return (val <= Integer.MAX_VALUE) ? (int) val : -1;
     }
+
+
 }
